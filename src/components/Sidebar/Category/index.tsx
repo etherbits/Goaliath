4bd5fc5691ea24105544398e0f942003e7ff5060
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-04-21 11:24:31 +0400

    adding a way to change icon colors

--- src/components/Sidebar/Category/index.tsx
+++ src/components/Sidebar/Category/index.tsx
@@ -1,5 +1,4 @@
 import { Category } from "@prisma/client";
-import Image from "next/image";
 
 interface Props {
   category: Category
@@ -7,7 +6,7 @@ interface Props {
 
 const Category: React.FC<Props> = ({ category }) => {
   return <div className="flex gap-2 w-60 p-1 rounded-md cursor-pointer hover:bg-neutral-800">
-    <Image width={24} height={24} src={category.icon} alt="category icon" />
+    <div className="w-6 h-6 bg-neutral-500" style={{ mask: `url(${category.icon}` }} />
     <span className="text-neutral-300">
       {category.name}
     </span>
